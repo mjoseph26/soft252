6e64ef2a3c4613595f632a628a030c421155271f
mjoseph26 <mjoseph082000@gmail.com>
2019-12-20 21:52:46 +0000

    added user account class

---
+++ MedicalSystem/src/medicalsystem/UserAccount.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medicalsystem;
+
+/**
+ *
+ * @author mjose
+ */
+public class UserAccount {
+    
+    private String id;
+    private String password;
+    private String name;
+    private String surname;
+    private String address;
+
+    public UserAccount(String id, String password, String name, String surname, String address) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+    }
+    
+//generate userId when account is created
+    //to create account take in values from text box. The id is the username and the password;
+    //this should only be done by the administrator.
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public static boolean checkDetails()
+    {
+        return true;
+    }
+    
+}
